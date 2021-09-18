5e43c78c2cd050fc47ba7e29be705a1ae0e27da2
公众号：前端学不动 <6666@66.com>
2021-09-18 15:16:20 +0800

    feat(stylelint): 允许CSS Module的一些特殊写法

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,8 +2,9 @@ module.exports = {
   extends: 'stylelint-config-standard',
   plugins: ['stylelint-order'],
   rules: {
-    'at-rule-no-unknown': [true, {
-      ignoreAtRules: ['tailwind', 'apply'],
-    }],
+    'at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind', 'apply'] }],
+    'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
+    'value-keyword-case': null,
   },
 };
