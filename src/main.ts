11a777495d1b9d92464f039f7baa14542abe5af7
公众号：前端学不动 <6666@66.com>
2021-10-13 12:54:37 +0800

    feat(home-page)

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import router from '@router';
-import App from './App';
-import 'css-doodle';
-import '@style/tailwind.css';
-import '@style/global.css';
-// import store from './store';
-
-createApp(App).use(router).mount('#app');
